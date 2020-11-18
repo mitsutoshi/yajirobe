9e98e949d4332a7664430d8f67b2ef39428ad54c
mitsutoshi <mitsu3204@gmail.com>
2020-11-18 23:42:59 +0900

    Improved notification

--- main.py
+++ main.py
@@ -33,13 +33,6 @@ def run():
         print(f"Existing order has been canceled. [id={o['id']}, product_id={o['product_id']}, side={o['side']}, quantity={o['quantity']}, price={o['price']}]")
 
     side = 'sell' if jpy_rate < fiat_rate else 'buy' if jpy_rate > fiat_rate else None
-    msg = f'''
-```
-Total: {int(blc_jpy + blc_btc_jpy):,} JPY
-  - {jpy_rate:.2%}: {int(blc_jpy):,} JPY
-  - {btc_rate:.2%}: {blc_btc} BTC ({int(blc_btc_jpy):,} JPY)
-```
-'''
     if side and abs(jpy_rate - fiat_rate) >= 0.01:
 
         # get quantity
@@ -49,17 +42,24 @@ Total: {int(blc_jpy + blc_btc_jpy):,} JPY
 
         # check order quantity
         if quantity < liquid.MIN_ORDER_QUANTITY:
-            print(f'Order was not sent as order quantity is less then {liquid.MIN_ORDER_QUANTITY}. [{quantity:.8f}]')
-            return
-
-        lqd.create_order(trade_pid, side, ltp, quantity)
-        msg += f'\nOrder has been created. [product_id={trade_pid}, side={side}, price={ltp}, quantity={quantity}]'
+            t = f'Order was not sent as order quantity {quantity:.8f} is less then {liquid.MIN_ORDER_QUANTITY}.'
+        else:
+            lqd.create_order(trade_pid, side, ltp, quantity)
+            t = f'Order has been created. [product_id={trade_pid}, side={side}, price={ltp}, quantity={quantity}]'
     else:
-        print('No need rebalancing.')
+        t = 'No need rebalancing.'
 
+    print(t)
+    msg = f'''{t}
+```
+Total: {int(blc_jpy + blc_btc_jpy):,} JPY
+  - {jpy_rate:.2%}: {int(blc_jpy):,} JPY
+  - {btc_rate:.2%}: {blc_btc} BTC ({int(blc_btc_jpy):,} JPY)
+```
+'''
     swu = os.getenv('SLACK_WEBHOOK_URL')
     if swu:
-        requests.post(swu, data = json.dumps({'text': msg}))
+        requests.post(swu, data=json.dumps({'text': msg}))
 
 
 if __name__ == '__main__':
