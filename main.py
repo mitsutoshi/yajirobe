61838b4db40a96286213d52d08910df83d6fcd97
mitsutoshi <mitsu3204@gmail.com>
2020-11-28 14:54:08 +0900

    Changed the stdout to log

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #! /usr/bin/env python
 import os
 import json
+import logging
 import requests
 from liquidpy.api import Liquid, MIN_ORDER_QUANTITY
+
+formatter = '%(levelname)s : %(asctime)s : %(message)s'
+logging.basicConfig(level=logging.INFO, format=formatter)
+logger = logging.getLogger()
 
 trade_pid = 5  # type: int
 fiat_rate= 0.5  # type: float
@@ -12,8 +17,8 @@ def run():
     lqd = Liquid(os.getenv('API_KEY'), os.getenv('API_SECRET'))  # type: Liquid
 
     # get current price of BTCJPY
-    ltp = float(lqd.get_product(trade_pid)['last_traded_price'])
-    print('Latest price of BTCJPY:', ltp)
+    ltp = float(lqd.get_products(product_id=trade_pid)['last_traded_price'])
+    logger.info(f'Latest price of BTCJPY: {ltp}')
 
     # get balance
     balance = lqd.get_accounts_balance()
@@ -25,12 +30,12 @@ def run():
             blc_btc_jpy = ltp * blc_btc
     jpy_rate = blc_jpy / (blc_jpy + blc_btc_jpy)
     btc_rate = blc_btc_jpy / (blc_jpy + blc_btc_jpy)
-    print(f'Balance: {int(blc_jpy)} JPY ({jpy_rate:.2%}), {blc_btc} BTC ({int(blc_btc_jpy)} JPY, {btc_rate:.2%}), Toal balance: {int(blc_jpy + blc_btc_jpy)} JPY')
+    logger.info(f'Balance: {int(blc_jpy)} JPY ({jpy_rate:.2%}), {blc_btc} BTC ({int(blc_btc_jpy)} JPY, {btc_rate:.2%}), Toal balance: {int(blc_jpy + blc_btc_jpy)} JPY')
 
     # cancel order if order exists
     for o in lqd.get_orders(status='live'):
         lqd.cancel_order(o['id'])
-        print(f"Existing order has been canceled. [id={o['id']}, product_id={o['product_id']}, side={o['side']}, quantity={o['quantity']}, price={o['price']}]")
+        logger.info(f"Existing order has been canceled. [id={o['id']}, product_id={o['product_id']}, side={o['side']}, quantity={o['quantity']}, price={o['price']}]")
 
     side = 'sell' if jpy_rate < fiat_rate else 'buy' if jpy_rate > fiat_rate else None
     if side and abs(jpy_rate - fiat_rate) >= 0.01:
@@ -38,7 +43,7 @@ def run():
         # get quantity
         ideal_blc_jpy = (blc_jpy + blc_btc_jpy) * fiat_rate
         quantity = round((ideal_blc_jpy - blc_jpy) / ltp, 8) if side == 'sell' else round((blc_jpy - ideal_blc_jpy) / ltp, 8)
-        print(f'The ideal balance of JPY fiat_rate is {fiat_rate}, so you should {side} {quantity:.8f} BTC ({int(quantity * ltp)} JPY).')
+        logger.info(f'The ideal balance of JPY fiat_rate is {fiat_rate}, so you should {side} {quantity:.8f} BTC ({int(quantity * ltp)} JPY).')
 
         # check order quantity
         if quantity < MIN_ORDER_QUANTITY:
@@ -49,7 +54,7 @@ def run():
     else:
         t = 'No need rebalancing.'
 
-    print(t)
+    logger.info(t)
     msg = f'''{t}
 ```
 Total: {int(blc_jpy + blc_btc_jpy):,} JPY
