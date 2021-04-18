45d550d6fd7a64ba51dd00591ce300d728497349
mitsutoshi <mitsu3204@gmail.com>
2021-04-19 03:52:32 +0900

    Fixed

--- get_balance.py
+++ get_balance.py
@@ -3,10 +3,11 @@ import os
 from datetime import datetime
 import time
 
-from liquidpy.api import Liquid
+from liquidpy.api import Liquid, PRODUCT_ID_ETHJPY
 from influxdb import InfluxDBClient
 
 
+first_record_created_at = 1604411435
 lqd = Liquid(os.getenv('API_KEY'), os.getenv('API_SECRET'))
 idb = InfluxDBClient(host=os.environ['DB_HOST'],
                      port=os.environ['DB_PORT'],
@@ -19,6 +20,18 @@ if __name__ == '__main__':
 
     balances = lqd.get_accounts_balance()
     products = lqd.get_products()
+    ethjpy = lqd.get_executions_me(product_id=PRODUCT_ID_ETHJPY, timestamp=first_record_created_at, limit=1000)
+
+    eth_buy_amount = sum([float(r['quantity']) * float(r['price']) for r in ethjpy if r['my_side'] == 'buy'])
+    eth_sell_amount = sum([float(r['quantity']) * float(r['price']) for r in ethjpy if r['my_side'] == 'sell'])
+    ignore_amount = eth_buy_amount - eth_sell_amount
+    print(f'ignore amount: {ignore_amount} JPY')
+
+    results = idb.query(f'select sum(amount) from deposits_history where time > \'2020-11-01\'')
+    deposit_sum = max([r[0]['sum'] for r in results])
+    captal = deposit_sum - ignore_amount
+    print(f'deposit amount: {deposit_sum} JPY')
+    print(f"captal: {captal}")
 
     # get latest position price
     executions = idb.query(f'select last(pos_price) from "executions"')
@@ -47,6 +60,7 @@ if __name__ == '__main__':
                     'amount': value,
                     'amount_jpy': amount_jpy,
                     'unrealized_pnl': amount_jpy - last_pos_price if b['currency'] == 'BTC' else None,
+                    'capital': captal,
                 }
             }
             points.append(p)
