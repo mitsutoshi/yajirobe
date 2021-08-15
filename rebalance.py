197146fd1ccd5bcf6af9d74cfe45bd9dcecd6a4e
mitsutoshi <mitsu3204@gmail.com>
2021-08-15 15:33:09 +0900

    Fixed notification message

--- rebalance.py
+++ rebalance.py
@@ -72,7 +72,7 @@ def main():
     lqd.cancel_all_orders()
 
     # create an order
-    t = 'No need to change alance.'
+    t = 'No need to change balance.'
     qty = estimate_order_qty(b_jpy, b_btc, ltp)
     logger.info(f'Estimated order quantity: {qty:.8f}')
     if qty > MIN_ORDER_QUANTITY:
